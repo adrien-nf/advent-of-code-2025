82300765f6cb53169495b901eb375b31e355eda2
Adrien N. F <iamnautilusjungle@gmail.com>
2025-12-05 14:36:17 +0100

    feat: update template

--- 00/second-puzzle.ts
+++ 00/second-puzzle.ts
@@ -1,5 +1,19 @@
 import fs from "fs";
 
-const input = fs.readFileSync("./01/input.txt").toString();
+const input = fs.readFileSync("./04/input.txt").toString();
 
-const lines = input.split("\n");
+const lines = input.split(/\r?\n/);
+
+class Solver {
+  constructor(lines: string[]) {
+    //
+  }
+
+  public solve(): number {
+    return 0;
+  }
+}
+
+const solver = new Solver(lines);
+
+console.log(solver.solve());
