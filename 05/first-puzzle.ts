6d9881d45a3124536a9387c23d06455a57fd1c8b
Adrien N. F <iamnautilusjungle@gmail.com>
2025-12-05 14:44:41 +0100

    feat: D05P01

--- 05/first-puzzle.ts
+++ 05/first-puzzle.ts
@@ -1,16 +1,61 @@
 import fs from "fs";
 
-const input = fs.readFileSync("./04/input.txt").toString();
+const input = fs.readFileSync("./05/input.txt").toString();
 
 const lines = input.split(/\r?\n/);
 
-class Solver {
-  constructor(lines: string[]) {
+class Range {
+  constructor(protected start: number, protected end: number) {
     //
   }
 
+  public includes(value: number): boolean {
+    return value >= this.start && value <= this.end;
+  }
+}
+
+class Fridge {
+  protected freshRanges: Range[] = [];
+
+  protected products: number[] = [];
+
+  constructor(protected lines: string[]) {
+    this.initializeRanges();
+    this.initializeProducts();
+  }
+
+  protected initializeRanges(): void {
+    let line;
+
+    do {
+      line = this.lines.shift();
+
+      const [start, end] = line.split("-");
+
+      this.freshRanges.push(new Range(Number(start), Number(end)));
+    } while (line !== "");
+  }
+
+  protected initializeProducts(): void {
+    this.products = this.lines.map((e) => Number(e));
+  }
+
+  public getFreshProducts(): number[] {
+    return this.products.filter((product) =>
+      this.freshRanges.some((range) => range.includes(product))
+    );
+  }
+}
+
+class Solver {
+  protected fridge: Fridge;
+
+  constructor(lines: string[]) {
+    this.fridge = new Fridge(lines);
+  }
+
   public solve(): number {
-    return 0;
+    return this.fridge.getFreshProducts().length;
   }
 }
 
